338e9bf9d129bf7044c3a08d06377999bfe6ca1a
Prem030 <premdh8@gmail.com>
2024-10-06 10:19:37 +0000

    drum kit JavaScript

---
+++ project 9 Drum Kit/index.js
@@ -0,0 +1,29 @@
+
+const html = document.querySelector('html');
+const keys = ['a' , 's' , 'd' , 'f' , 'g' , 'h' , 'j' , 'k' , 'l']
+let key
+html.addEventListener('keydown' , (event) =>{
+    
+    keys.forEach( element =>{
+        if(element === event.key){
+            key = document.getElementById(`${event.keyCode}`)
+            
+            const audio = new Audio(`sounds/${event.keyCode}.wav`);
+            audio.play();
+            key.classList.add('play')
+            console.log(key);
+        }
+            
+    })
+})
+
+const allButton = document.querySelectorAll('.btn');
+html.addEventListener('keyup' , () =>{
+    keys.forEach( element =>{
+        allButton.forEach(element =>{
+            element.classList.remove('play')
+        })
+            
+    })
+    
+})
